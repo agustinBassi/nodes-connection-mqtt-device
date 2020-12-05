287054603bb0b617dbee2657f1b7796d4935d108
Agustin <jagustinbassi@gmail.com>
2020-12-05 12:27:32 -0300

    Adapt code to connect to AWS IoT Core based in its documentation

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 // Author:  Agustin Bassi (https://github.com/agustinBassi)
 // Licence: GPLV3+
 // Version: 1.0.0
-// Date:    November 2020
+// Date:    December 2020
 /*==================[inclusions]=============================================*/
 
+#include "secrets.h"
+#include <WiFiClientSecure.h>
+#include <MQTTClient.h>
 #include <ArduinoJson.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
+#include "WiFi.h"
 
 /*==================[macros and definitions]=================================*/
 
@@ -16,56 +18,37 @@
 #define LED_ONBOARD                 2
 #define BUTTON_ONBOARD              0
 #define INIT_DELAY                  3000
+#define LOOP_DELAY                  1000
 #define SERIAL_BAURDATE             115200
 #define WIFI_CONNECTION_DELAY       500
-#define MQTT_RETRY_CONNECTION_DELAY 5000
+#define MQTT_RETRY_CONNECTION_DELAY 100
 #define BLINK_TIME                  100
 #define DEFAULT_PUBLISH_TIME        5000
 #define MIN_PUBLISH_TIME            1000
 #define MAX_PUBLISH_TIME            30000
 #define DEFAULT_TEMPERATURE_VALUE   25
 #define DEFAULT_HUMIDITY_VALUE      50
+#define MQTT_BUFFER_SIZE            256
+// MQTT TOPICS
+#define AWS_IOT_PUBLISH_TOPIC       "esp32/pub"
+#define AWS_IOT_SUBSCRIBE_TOPIC     "esp32/sub"
 
 /*==================[internal data declaration]==============================*/
 
-// Client to establish WiFi connection
-WiFiClient WifiClient;
-// Client MQTT, needs a WiFi connection
-PubSubClient MqttClient(WifiClient);
-// Variable to change report time
-static uint32_t PublishTime = DEFAULT_PUBLISH_TIME;
-// Variables to store sensor reads values
-static uint16_t TemperatureValue = DEFAULT_TEMPERATURE_VALUE;
-static uint16_t HumidityValue = DEFAULT_HUMIDITY_VALUE;
+WiFiClientSecure WiFiSecureClient = WiFiClientSecure();
+MQTTClient MqttClient = MQTTClient(MQTT_BUFFER_SIZE);
 
 /*==================[internal functions declaration]=========================*/
 
-void Gpio_BlinkLed           (uint8_t led, uint32_t milliseconds);
-void Wifi_EstablishConnection(void);
-void Mqtt_ConnectToBroker    (void);
-void Mqtt_PublishTopic       (String topic, String payload);
-void Mqtt_SubscribeCallback  (char* topic, byte* payload, unsigned int length);
-void App_Init                (void);
-void App_PublishStatus       (void);
-void App_UpdateSensorData    (void);
-void App_Loop                (void);
-
-/*==================[internal data definition]===============================*/
-
-// Device indentification
-const String DEVICE_ID           = "esp32-001";
-// Wifi settings
-const String WIFI_SSID           = "WIFI_SSID";
-const String WIFI_PASS           = "WIFI_PASS";
-// Mqtt server settings
-const String MQTT_SERVER         = "192.168.1.1";
-const int    MQTT_PORT           = 1883;
-const String MQTT_USER           = "";
-const String MQTT_PASS           = "";
-// Mqtt message settings
-const String MQTT_TOPIC_INIT     = "device/up/" + DEVICE_ID;
-const String MQTT_TOPIC_STATUS   = "device/status/" + DEVICE_ID;
-const String MQTT_TOPIC_CONFIG   = "device/config/" + DEVICE_ID;
+void Gpio_BlinkLed              (uint8_t led, uint32_t milliseconds);
+void Wifi_EstablishConnection   (void);
+void Wifi_SetSecureCertificates (void);
+void Mqtt_ConnectToBroker       (void);
+void Mqtt_PublishTopic          (const char topic[], const char payload[]);
+void Mqtt_SubscribeCallback     (String &topic, String &payload);
+void App_Init                   (void);
+void App_PublishStatus          (void);
+void App_Loop                   (void);
 
 /*==================[external data definition]===============================*/
 
@@ -84,7 +67,8 @@ void Wifi_EstablishConnection(){
     Serial.print("Connecting to ");
     Serial.print(WIFI_SSID);
     // Try to connect
-    WiFi.begin(WIFI_SSID.c_str(), WIFI_PASS.c_str());
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
     // Wait until connection is established
     while (WiFi.status() != WL_CONNECTED) {
         delay(WIFI_CONNECTION_DELAY);
@@ -96,68 +80,47 @@ void Wifi_EstablishConnection(){
     Serial.println(WiFi.localIP());
 }
 
-void Mqtt_ConnectToBroker(){
-    // Loop until we're reconnected
-    while (!MqttClient.connected()) {
-        Serial.print("Attempting MQTT connection...");
-        // Attempt to connect
-        if (MqttClient.connect(DEVICE_ID.c_str())) {
-            Serial.println("connected");
-            // Subscribe to topic
-            MqttClient.subscribe(MQTT_TOPIC_CONFIG.c_str());
-            Serial.print("Subscribed to topic: ");
-            Serial.println(MQTT_TOPIC_CONFIG.c_str());
-            // Advice to broker that device is running correctly
-            Mqtt_PublishTopic(MQTT_TOPIC_INIT, "up");
-        } else {
-            Serial.print("failed, rc = ");
-            Serial.print(MqttClient.state());
-            Serial.println(". Try again in MQTT_RETRY_CONNECTION_TIME ms.");
-            // Wait 5 seconds before retrying
-            delay(MQTT_RETRY_CONNECTION_DELAY);
-        }
-    }
+void Wifi_SetSecureCertificates(){
+    // Configure WiFiClientSecure to use the AWS IoT device credentials
+    WiFiSecureClient.setCACert(AWS_CERT_CA);
+    WiFiSecureClient.setCertificate(AWS_CERT_CRT);
+    WiFiSecureClient.setPrivateKey(AWS_CERT_PRIVATE);
 }
 
-void Mqtt_PublishTopic(String topic, String payload){
+void Mqtt_ConnectToBroker(){
+    // Connect to the MQTT broker on the AWS endpoint we defined earlier
+    MqttClient.begin(AWS_IOT_ENDPOINT, 8883, WiFiSecureClient);
+    // Create a message handler
+    MqttClient.onMessage(Mqtt_SubscribeCallback);
+    Serial.print("Attempting MQTT connection...");
+    while (!MqttClient.connect(THINGNAME)){
+        Serial.print(".");
+        Gpio_BlinkLed(LED_ONBOARD, 100);
+        delay(MQTT_RETRY_CONNECTION_DELAY);
+    }
+    if (!MqttClient.connected()){
+        Serial.println("\n\rAWS IoT Timeout!");
+        return;
+    }
+    // Subscribe to topic
+    MqttClient.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+    Serial.print("\n\rSubscribed to topic: ");
+    Serial.println(AWS_IOT_SUBSCRIBE_TOPIC);
+    Serial.println("Correctly connected to Broker!");
+}
+
+void Mqtt_PublishTopic(const char topic[], const char payload[]){
     // Print in console the topic-payload that will be sent
     Serial.print("Sending MQTT Topic-Payload: ");
     Serial.print(topic);
     Serial.print(" -> ");
     Serial.println(payload);
     // Publish message
-    MqttClient.publish( topic.c_str(), payload.c_str(), true );
+    MqttClient.publish(topic, payload);
 }
 
-void Mqtt_SubscribeCallback(char* topic, byte* payload, unsigned int length){
-    if (strcmp(topic, MQTT_TOPIC_CONFIG.c_str()) == 0){
-        StaticJsonDocument<100> doc;
-        // put null char to payload buffer
-        payload[length] = '\0';
-        // Test if parsing succeeds.
-        DeserializationError error = deserializeJson(doc, payload);
-        if (error) {
-            Serial.print("deserializeJson() failed: ");
-            Serial.println(error.f_str());
-            return;
-        }
-        // obtain the value from the received JSON
-        uint32_t publishTime = doc["publish_secs"];
-        // convert seconds to milliseconds
-        publishTime *= 1000;
-        // Check if value received is correct.
-        if (publishTime >= MIN_PUBLISH_TIME && publishTime <= MAX_PUBLISH_TIME){
-            Serial.print("Publish time will change to ");
-            Serial.print(publishTime);
-            Serial.println(" ms");
-            // Change publish time
-            PublishTime = publishTime;
-        } else {
-            Serial.println("Invalid publish time, must be between MIN_REPORT_TIME and MAX_REPORT_TIME ms.");
-        }
-    } else {
-        Serial.println("Unknown topic received!");
-    }
+void Mqtt_SubscribeCallback(String &topic, String &payload){
+    Serial.println("[Mqtt_SubscribeCallback] " + topic + " -> " + payload);
 }
 
 void App_Init(){
@@ -169,64 +132,30 @@ void App_Init(){
     pinMode(LED_ONBOARD, OUTPUT);
     pinMode(BUTTON_ONBOARD, INPUT);
     // print to console Init message
-    Serial.println("Welcome to Nodes Connection MQTT device!");
-    // Set MQTT Server
-    MqttClient.setServer(MQTT_SERVER.c_str(), MQTT_PORT);
-    // Configure a callback to receive topics
-    MqttClient.setCallback(Mqtt_SubscribeCallback);
+    Serial.println("Welcome to AWS Connection MQTT device!");
     // Connect to WiFi
     Wifi_EstablishConnection();
+    // Configure WiFiClientSecure to use the AWS IoT device credentials
+    Wifi_SetSecureCertificates();
+    // Establish a connection to the broker
+    Mqtt_ConnectToBroker();
     // Leave built in led on
     digitalWrite(LED_ONBOARD, false);
 }
 
-void App_PublishStatus(void){
-    StaticJsonDocument<100> doc;
-    char sensorValueStr [6];
-    char payloadBuffer [100];
-    // Convert int TemperatureValue to str
-    itoa(TemperatureValue, sensorValueStr, 10);
-    doc["temperature"] = sensorValueStr;
-    // Convert int TemperatureValue to str
-    itoa(HumidityValue, sensorValueStr, 10);
-    doc["humidity"] = sensorValueStr;
-    // serialize JSON in order to send it as MQTT payload
-    serializeJson(doc, payloadBuffer);
-    Mqtt_PublishTopic(MQTT_TOPIC_STATUS, payloadBuffer);
-    // Blink LED to testify the topic sent
-    Gpio_BlinkLed(LED_ONBOARD, BLINK_TIME);
-}
-
-void App_UpdateSensorData(void){
-    Serial.println("Updating the sensor data values");
-    TemperatureValue = random(0, 50);
-    HumidityValue = random(0, 100);
+void App_PublishStatus(){
+    StaticJsonDocument<200> doc;
+    doc["time"] = millis();
+    doc["sensor_a0"] = random(0, 255);
+    char jsonBuffer[512];
+    serializeJson(doc, jsonBuffer);
+    Mqtt_PublishTopic(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
 }
 
 void App_Loop(){
-    // Create a variable to publish topic
-    static uint32_t tickCounter = 0;
-    // Check if MQTT client is not connected to server.
-    if (!MqttClient.connected()) {
-        // Try to connect with MQTT Server.
-        Mqtt_ConnectToBroker();
-    }
-    // Loop for incoming messages.
+    App_PublishStatus();
     MqttClient.loop();
-    // Use this variable to unblock the loop
-    if (++tickCounter >= PublishTime){
-        // Reset counter
-        tickCounter = 0;
-        // send the current status to the broker MQTT
-        App_PublishStatus();
-    }
-    // if button is pressed change the current fake sensor values
-    if(!digitalRead(BUTTON_ONBOARD)){
-        App_UpdateSensorData();
-        delay(1000);
-    }
-    // delay 1 MS to start loop again
-    delay(1);
+    delay(LOOP_DELAY);
 }
     
 /*==================[external functions definition]==========================*/
